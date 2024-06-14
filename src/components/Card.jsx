101e694e3db0da1f84089da0e18bb4d13c26e2be
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-06-14 22:44:13 +0700

    textarea touchup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card(props) {
   return (
     <>
       <button
-        className="flex items-center rounded-3xl bg-bluefield p-5 min-w-[30vh] my-3 overflow-hidden font-sans"
+        className="flex items-center rounded-3xl bg-bluefield py-5 min-w-full my-3 px-10 overflow-hidden font-sans"
         onClick={props.handleClick}
       >
         <img
