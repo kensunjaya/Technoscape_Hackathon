9f2e9e9527521ba1c15fe3fd26fddbe9ef99c714
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-06-15 10:39:58 +0700

    data limitation, added history

---
+++ src/assets/InformationData.js
@@ -0,0 +1,40 @@
+export const information = 
+  [
+    {
+      role: "user",
+      parts: [
+        { text: "Apa saja program dan prospek karir di Bina Nusantara (BINUS) university fakultas School of Computer Science?"}
+      ]
+    },
+    {
+      role: "model",
+      parts: [
+        { text: "Program Description: Memiliki pemahaman dan penguasaan Computer Science (Teknik Informatika) merupakan sebuah kunci utama dalam menghadapi tantangan dunia global saat ini. Perkembangan yang sangat cepat di berbagai sektor industri dan ICT membutuhkan banyak sumber daya manusia (SDM)/ talent yang berperan penting sebagai inisiator teknologi dengan solusi yang kreatif. Program Computer Science menekankan pada beberapa bidang spesifik yang menjadi pilihan bagi mahasiswa, diantaranya Intelligent Systems, Database Technology, Interactive Multimedia, Software Engineering, dan Network Technology. Prospective Career:Software Engineer/Developer, Machine Learning Engineer, Data Scientist/Analyst, Web Engineer/ Developer, Mobile Developer, System Analyst/Developer, Network Specialist, Database Specialist, IT Consultant, Multimedia Programmer, Lecturer/Trainer, Techno-preneur" }
+      ]
+    },
+    {
+      role: "user",
+      parts: [
+        { text: "Data diri: Nama Lengkap: Kenneth Sunjaya, Email: kennethsunjaya@gmail.com"}
+      ]
+    },
+    {
+      role: "model",
+      parts: [
+        { text: "Data diri berhasil kami ingat, Kami akan memberikan informai seputar Binus University"}
+      ]
+    },
+    {
+      role: "user",
+      parts: [
+        { text: "Tema pembicaraan kita adalah seputar Binus University (Bina Nusantara University)"}
+      ]
+    },
+    {
+      role: "model",
+      parts: [
+        { text: "Baik, mulai dari sekarang saya akan hanya membahas seputar Bina Nusantara University. Saya tidak akan menjawab pertanyaan di luar topik Binus University"}
+      ]
+    }
+    // ini nanti diisi sama info info binus
+  ]
